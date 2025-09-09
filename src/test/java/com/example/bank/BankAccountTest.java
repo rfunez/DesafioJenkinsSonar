ceb6fe49da2190e0449e3785e9f6ed929fed5ae6
rfunez <rfunez1972@gmail.com>
2025-09-09 19:09:39 +0200

    First commit

---
+++ src/test/java/com/example/bank/BankAccountTest.java
@@ -0,0 +1,43 @@
+package com.example.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankAccountTest {
+
+    @Test
+    void testInitialBalance() {
+        BankAccount account = new BankAccount(100.0);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        BankAccount account = new BankAccount(50.0);
+        account.deposit(25.0);
+        assertEquals(75.0, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        BankAccount account = new BankAccount(100.0);
+        account.withdraw(30.0);
+        assertEquals(70.0, account.getBalance());
+    }
+
+    @Test
+    void testWithdrawInsufficientFunds() {
+        BankAccount account = new BankAccount(20.0);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            account.withdraw(30.0);
+        });
+        assertEquals("Insufficient funds", exception.getMessage());
+    }
+
+    @Test
+    void testHasSufficientFunds() {
+        BankAccount account = new BankAccount(200.0);
+        assertTrue(account.hasSufficientFunds(100.0));
+        assertFalse(account.hasSufficientFunds(300.0));
+    }
+}
